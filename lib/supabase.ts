775b2a671279db77b89dcc75a953e5b629f59766
Nuruddin6624 <87517053+Nuruddin6624@users.noreply.github.com>
2026-03-30 11:22:50 +0600

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // REPLACE THESE WITH YOUR ACTUAL SUPABASE PROJECT DETAILS
 const SUPABASE_URL = 'https://vkcgcrwxippesquiibko.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_0XhDX7nNvcx6RrQbJIBU1g_bNIsjfcf';
+const SUPABASE_ANON_KEY = 'sb_publishable_hiohHuDbffWMxHxUKyxtHA_xrPCCZqx';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
